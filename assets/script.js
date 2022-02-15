f2fde62dd67600501aced805c2f9cf950a162d23
Mahmoud Sabbagh <mahmoudsabbagh8@gmail.com>
2022-02-15 14:40:14 -0500

    Initial

---
+++ assets/script.js
@@ -0,0 +1,100 @@
+const homebtn  = document.getElementById('home');
+const edubtn = document.getElementById('education');
+const workbtn = document.getElementById('workexp');
+const mbtn = document.getElementById('more');
+
+const homecontent = document.querySelector('.homecontent');
+const educationcontent = document.querySelector('.educationcontent');
+const workcontent = document.querySelector('.workexpcontent');
+const morecontent = document.querySelector('.morecontent');
+
+const rotation = document.querySelector('.circle');
+
+var degree = 0;
+
+homebtn.addEventListener('click', () => {
+    scroller(degree);
+    degree+=90;
+    return degree;
+});
+edubtn.addEventListener('click', () => {
+    scroller(degree);
+    degree+=90;
+    return degree;
+});
+workbtn.addEventListener('click',() => {
+    scroller(degree);
+    degree+=90;
+    return degree;
+});
+mbtn.addEventListener('click',() => {
+    scroller(degree);
+    degree=0;
+    return degree;
+});
+
+function scroller(deg){
+
+    switch(deg){
+        case 0 : 
+            if($(window).scrollTop() < 2019  || $(window).scrollTop() >= 3938){
+                rotation.style.transform = `rotate(-${deg+90}deg)`;
+                educationcontent.scrollIntoView({behavior:"smooth"});
+            }           
+            console.log(deg);
+            break;        
+        case 90:
+            if($(window).scrollTop() < 3938 || $(window).scrollTop() >= 5857){
+                rotation.style.transform = `rotate(-${deg+90}deg)`;
+                workcontent.scrollIntoView({behavior: "smooth"});
+            }            
+            console.log(deg);
+            break;
+        case 180:
+            if($(window).scrollTop() < 5857){
+                rotation.style.transform = `rotate(-${deg+90}deg)`;
+                morecontent.scrollIntoView({behavior:"smooth"});
+            }           
+            console.log(deg);
+            break;
+        case 270:
+            if($(window).scrollTop() >= 2019){
+                rotation.style.transform = `rotate(0deg)`;
+                homecontent.scrollIntoView({behavior:"smooth"});
+            }
+            console.log(deg);
+            break;
+        default:
+            console.log("Something broke, reached default switch!");
+            break;
+    } 
+     
+}
+
+
+
+//  0
+//  2019
+//  3938
+//  5857
+
+
+
+$(window).on("scroll",function() {
+    if ($(document).scrollTop() > 0 && $(document).scrollTop()<2019) {
+      rotation.style.transform = `rotate(0)`;
+      console.log('called step1');
+    }else if($(document).scrollTop()>=2018 && $(document).scrollTop()<3937){
+        rotation.style.transform = `rotate(-90deg)`;
+        console.log('called step2');
+    }else if($(document).scrollTop() >= 3937 && $(document).scrollTop() < 5856){
+        rotation.style.transform = `rotate(-180deg)`;
+        console.log('called step3');
+    }else if($(document).scrollTop()>=5856){
+        rotation.style.transform = `rotate(-270deg)`;
+        console.log('called step4');
+    }else{
+       rotation.style.transform = `rotate(0deg)`;
+    }
+    console.log($(document).scrollTop());
+  });
